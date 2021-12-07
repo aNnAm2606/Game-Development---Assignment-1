76238acb5506d75915ca512c7b09df962a99e93d
migon25 <miguel.garon@gmail.com>
2021-12-07 20:10:29 +0100

    fixed issue with logo screen

--- Plant Collector platformerGame/Game/Source/Logo.cpp
+++ Plant Collector platformerGame/Game/Source/Logo.cpp
@@ -14,12 +14,6 @@
 Logo::Logo(bool startEnabled) : Module(startEnabled)
 {
 	name.Create("logo");
-
-	exist.PushBack({ 0, 0, 16, 16 });
-	exist.PushBack({ 0, 0, 16, 16 });
-	exist.loop = true;
-	exist.speed = 0.2f;
-
 }
 
 Logo::~Logo()
@@ -32,8 +26,9 @@ bool Logo::Awake(pugi::xml_node& config)
 	LOG("Loading Logo");
 	bool ret = true;
 
-	screenImage.Create(config.child("Logo").child_value());
+	screenImage.Create(config.child("background").child_value());
 	StartButton.Create(config.child("spaceToStart").child_value());
+	logoIcon.Create(config.child("icon").child_value());
 
 	return ret;
 }
@@ -45,81 +40,20 @@ bool Logo::Start()
 
 	bool ret = true;
 	background = app->tex->Load(screenImage.GetString());
+	space = app->tex->Load(StartButton.GetString());
 	icon = app->tex->Load(logoIcon.GetString());
 
-	// stating animation
-	cPos.x = 0;
-	cPos.y = 260;
-
-	LOG("settting camera to (0,0)");
 	app->render->camera.x = 0;
 	app->render->camera.y = 0;
 
 	return ret;
 }
 
-bool Logo::LoadState(pugi::xml_node& data)
-{
-	level1 = data.child("Level").attribute("level1").as_bool();
-	level2 = data.child("Level").attribute("level1").as_bool();
-
-	return true;
-}
-
-bool Logo::SaveState(pugi::xml_node& data) const
-{
-	if (app->level1->isEnabled == true && app->level1->F6Load == true)
-	{
-		data.child("Level").attribute("level1").set_value(true);
-		LOG("level1 Save is TRUE");
-	}
-	else
-	{
-		data.child("Level").attribute("level1").set_value(false);
-		LOG("level1 Save is FALSE");
-	}
-
-	//if (app->level2->isEnabled == true && app->level2->F6Load == true)
-	//{
-	//	data.child("Level").attribute("level2").set_value(true);
-	//	LOG("level2 Save is TRUE");
-	//}
-	//else
-	//{
-	//	data.child("Level").attribute("level2").set_value(false);
-	//	LOG("level2 Save is FALSE");
-	//}
-
-	return true;
-}
 
 bool Logo::Update(float dt)
 {
 	bool ret = true;
-	if (app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
-	{
-		app->fade->Fade_To_Black(this, (Module*)app->sceneIntro, 180);
-	}
-
-	//if (app->input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN)
-	//{
-	//	if (level1 == true)
-	//	{
-	//		app->level1->F6Load = true;
-	//		app->level2->F6Load = false;
-	//		app->fade->Fade_To_Black(this, (Module*)app->level1);
-	//	}
-	//	else if (level2 == true)
-	//	{
-	//		app->level2->F6Load = true;
-	//		app->level1->F6Load = false;
-	//		app->fade->Fade_To_Black(this, (Module*)app->level2);
-	//	}
-	//}
-	currentAnimation->Update();
-
-	cPos.x += 5;
-
+	app->fade->Fade_To_Black(this, (Module*)app->sceneIntro, 240);
 	return ret;
 }
 
@@ -127,9 +61,8 @@ bool Logo::PostUpdate()
 {
 	frames++;
 	app->render->DrawTexture(background, 0, 0, NULL);
-
-	SDL_Rect rect = currentAnimation->GetCurrentFrame();
-	app->render->DrawTexture(icon, cPos.x, cPos.y, &rect);
+	//app->render->DrawTexture(space, 10, 0, NULL);
+	app->render->DrawTexture(icon, 480, 270, NULL);
 
 	return true;
 }
@@ -138,6 +71,7 @@ bool Logo::CleanUp()
 {
 	LOG("Freeing Logo");
 	app->tex->UnLoad(background);
+	app->tex->UnLoad(space);
 	app->tex->UnLoad(icon);
 	return true;
 }
--- Plant Collector platformerGame/Game/Source/Logo.h
+++ Plant Collector platformerGame/Game/Source/Logo.h
@@ -27,10 +27,6 @@ public:
 	// Updates the scene's background animations
 	bool Update(float dt) override;
 
-	// Load last game
-	bool LoadState(pugi::xml_node&);
-	bool SaveState(pugi::xml_node&) const;
-
 	// Called at the end of the application loop.
 	// Performs the render call of all the parts of the scene's background
 	bool PostUpdate() override;
@@ -39,20 +35,13 @@ public:
 public:
 	// The scene sprite sheet loaded into an SDL_Texture
 	SDL_Texture* background;
+	SDL_Texture* space;
 	SDL_Texture* icon;
-
 
 	SString screenImage;
 	SString StartButton;
 	SString logoIcon;
 
-	Animation exist;
-	Animation* currentAnimation = nullptr;
-
-	iPoint cPos;
-
-	bool level1;
-	bool level2;
 	int frames;
 };
 
